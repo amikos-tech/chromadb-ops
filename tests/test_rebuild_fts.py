2f807ba726196fc8c98922403d73ecb269eb3b07
Trayan Azarov <trayan.azarov@amikos.tech>
2024-12-02 20:15:02 +0200

    fix: Fixing FTS rebuild

--- tests/test_rebuild_fts.py
+++ tests/test_rebuild_fts.py
@@ -11,16 +11,17 @@ from chroma_ops.rebuild_fts import rebuild_fts
 
 
 def test_rebuild_fts() -> None:
-    records_to_add = 1
+    records_to_add = 100
     with tempfile.TemporaryDirectory() as temp_dir:
         client = chromadb.PersistentClient(path=temp_dir)
         col = client.create_collection("test")
         ids_documents = [
-            (f"{uuid.uuid4()}", f"document {i}", [0.1] * 1536)
+            (f"{uuid.uuid4()}", f"document {i}" if i % 2 == 0 else None, [0.1] * 384)
             for i in range(records_to_add)
         ]
         ids, documents, embeddings = zip(*ids_documents)
         col.add(ids=list(ids), documents=list(documents), embeddings=list(embeddings))
+        res_before = col.get(where_document={"$contains": "document 1"})
         sql_file = os.path.join(temp_dir, "chroma.sqlite3")
         conn = sqlite3.connect(sql_file)
         cursor = conn.cursor()
@@ -36,4 +37,5 @@ def test_rebuild_fts() -> None:
         shutil.copytree(temp_dir, fixed_temp_dir)
         client = chromadb.PersistentClient(path=fixed_temp_dir)
         col = client.get_collection("test")
-        col.get(where_document={"$contains": "document 0"})
+        res_after = col.get(where_document={"$contains": "document 1"})
+        assert res_after == res_before
