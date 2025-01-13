218906df5c58aadbb0a842a5c236bf4f67f3bfb2
Trayan Azarov <trayan.azarov@amikos.tech>
2025-01-13 08:52:36 +0200

    fix: Linting

--- chroma_ops/db.py
+++ chroma_ops/db.py
@@ -5,7 +5,8 @@ from chroma_ops.db_info import command as db_info_command
 db_commands = typer.Typer(no_args_is_help=True)
 
 db_commands.command(
-    name="info", help="Provide persistent Chroma DB information. "
+    name="info",
+    help="Provide persistent Chroma DB information. "
     "Useful to understand how your Chroma works or get support from the team.",
     no_args_is_help=True,
 )(db_info_command)
